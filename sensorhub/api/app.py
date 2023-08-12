a0b4370c1b97f29ddf947fe4c2f76e757c81ed07
Juan Ortiz <notjuanortiz@gmail.com>
2023-08-12 17:00:31 -0400

    Reduce online_time: 1 minute -> 10 seconds
    Signed-off-by: Juan Ortiz <notjuanortiz@gmail.com>

--- sensorhub/api/app.py
+++ sensorhub/api/app.py
@@ -40,7 +40,7 @@ def get_sensor_list():
                         S.sensor_id,
                         S.sensor_name,
                         S.manufacturer_id,
-                        CASE WHEN max(SD.sensor_time) >= now() - interval '1 minutes' THEN TRUE ELSE FALSE END AS is_online
+                        CASE WHEN max(SD.sensor_time) >= now() - interval '10 seconds' THEN TRUE ELSE FALSE END AS is_online
                     FROM
                         sensors AS S
                     JOIN
